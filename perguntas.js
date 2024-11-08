1adaae987bf4a8dddcbf3bd41e6031cbda5a7ac3
HenriqueLC15 <00001090461501sp@al.educacao.sp.gov.br>
2024-11-08 11:38:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de são paulo?',
+    'A capital de são paulo é são paulo',
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa',
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)',
+)
+
+criaCartao(
+    'Alzheimer',
+    'o que e uma doeça neurodegenerativa?',
+    'uma perca progresiva da estrutura ou funcionamento dos neuronios',
+)
